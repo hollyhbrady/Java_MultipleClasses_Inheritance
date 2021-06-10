b30fe17df703da0cad7d7475517340d6230a06a9
hollyhbrady <hollyhbrady@gmail.com>
2021-06-10 14:43:42 +0100

    Booking Class

---
+++ src/main/java/Booking.java
@@ -0,0 +1,11 @@
+public class Booking {
+
+    private Bedroom bedroom;
+    private int nightsBooked;
+
+    public Booking(Bedroom bedroom, int nightsBooked) {
+        this.bedroom = bedroom;
+        this.nightsBooked = nightsBooked;
+    }
+
+}
